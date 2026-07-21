77f6eaeab4d0d2199538cb263ff1f16dbd3b9ab5
stephanieoghenemega-eng <stephanieoghenemega@gmail.com>
2026-07-21 13:10:40 +0100

    chore: setup React Query for data fetching migration (fixes #58) (#61)
    Co-authored-by: Jaiyeola Akinjide <jaiyeolawety705@gmail.com>

--- components/Providers.tsx
+++ components/Providers.tsx
@@ -1,11 +1,14 @@
 'use client';
 
 import { WalletProvider } from '@/contexts/WalletContext';
+import { ReactQueryProvider } from '@/components/ReactQueryProvider';
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <WalletProvider>
-      {children}
-    </WalletProvider>
+    <ReactQueryProvider>
+      <WalletProvider>
+        {children}
+      </WalletProvider>
+    </ReactQueryProvider>
   );
 }
