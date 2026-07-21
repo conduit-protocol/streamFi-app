9e12a8b270057ede867c9959853957b34cc44e69
bade2brazy <ajibadeimeokparia@gmail.com>
2026-07-21 13:32:56 +0100

    feat: implement global transaction manager & toasts (fixes #64) (#65)
    Co-authored-by: Jaiyeola Akinjide <jaiyeolawety705@gmail.com>

--- components/Providers.tsx
+++ components/Providers.tsx
@@ -2,12 +2,14 @@
 
 import { WalletProvider } from '@/contexts/WalletContext';
 import { ReactQueryProvider } from '@/components/ReactQueryProvider';
+import { Toaster } from 'react-hot-toast';
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <ReactQueryProvider>
       <WalletProvider>
         {children}
+        <Toaster position="bottom-right" />
       </WalletProvider>
     </ReactQueryProvider>
   );
